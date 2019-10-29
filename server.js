6951a059157ff9ffb584132a5dbfd1a2e3196b13
Gabriel Ifeanyi <gabriel_ifeanyi@ymail.com>
2019-10-29 20:20:13 +0100

    fixed an error in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/sfs-angular'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/sfs-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/construmart-admin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
