ef10f3102fd0b10680d82ec7a778adfba0268bb9
Gabriel Ifeanyi <gabriel_ifeanyi@ymail.com>
2019-10-29 20:28:24 +0100

    fixed an error in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sfs-angular'));
+app.use(express.static('./dist/construmart-admin'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/construmart-admin/index.html'));
